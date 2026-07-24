5b2d546c24901a9f1fa80de371e31a827f4da3e7
fantasykurume <fantasykurume0820@gmail.com>
2026-07-25 07:36:47 +0900

    Add initShop function to main.js initialization

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@ import { fetchAll } from "./api/api.js";
 import { initMenu } from "./modules/menu.js";
 import { initScroll } from "./modules/scroll.js";
 import { initModal } from "./components/modal.js";
-
+import { initShop } from "./modules/shop.js";
 import { initHero } from "./modules/hero.js";
 import { initGallery } from "./modules/gallery.js";
 import { initNews } from "./modules/news.js";
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded", async()=>{
     }
 
     const shop = data.shop[0];
-
+    initShop(shop);
     initHero(shop);
     initGallery(data.gallery);
     initNews(data.news);
