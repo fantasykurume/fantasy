2f3f12f95111b7d553fe340df72269e6d682ff9c
fantasykurume <fantasykurume0820@gmail.com>
2026-07-25 22:05:37 +0900

    Update auth.js

--- assets/js/admin/auth.js
+++ assets/js/admin/auth.js
@@ -5,65 +5,25 @@
 
 import { CONFIG } from "../config/config.js";
 
+document.addEventListener("DOMContentLoaded",()=>{
 
-document.addEventListener(
-"DOMContentLoaded",
-()=>{
-
-
-const loginButton =
-document.getElementById("loginButton");
-
-
-if(!loginButton) return;
-
+const loginButton=document.getElementById("loginButton");
+if(!loginButton)return;
 
 loginButton.onclick=async()=>{
 
-
-const username =
-document.getElementById("loginId")
-.value.trim();
-
-
-const password =
-document.getElementById("loginPassword")
-.value.trim();
-
-
-const message =
-document.getElementById("loginMessage");
-
-
+const username=document.getElementById("loginId").value.trim();
+const password=document.getElementById("loginPassword").value.trim();
+const message=document.getElementById("loginMessage");
 
 try{
 
+const form=new FormData();
+form.append("action","loginAdmin");
+form.append("username",username);
+form.append("password",password);
 
-const form =
-new FormData();
-
-
-form.append(
-"action",
-"loginAdmin"
-);
-
-
-form.append(
-"username",
-username
-);
-
-
-form.append(
-"password",
-password
-);
-
-
-
-const response =
-await fetch(
+const response=await fetch(
 CONFIG.API_URL,
 {
 method:"POST",
@@ -71,11 +31,7 @@ body:form
 }
 );
 
-
-
-const result =
-await response.json();
-
+const result=await response.json();
 
 
 if(
@@ -83,81 +39,31 @@ result.status==="success" &&
 result.data.login
 ){
 
-
-localStorage.setItem(
-"fantasy_admin",
-"true"
-);
-
-
-localStorage.setItem(
-"admin_name",
-result.data.name
-);
-
-
-localStorage.setItem(
-"admin_role",
-result.data.role
-);
-
-
-localStorage.setItem(
-"admin_token",
-result.data.token
-);
-
-
+localStorage.setItem("fantasy_admin","true");
+localStorage.setItem("admin_name",result.data.name);
+localStorage.setItem("admin_role",result.data.role);
+localStorage.setItem("admin_token",result.data.token);
 localStorage.setItem(
 "must_change_password",
-String(
-result.data.must_change_password
-)
+String(result.data.must_change_password)
 );
 
-
-
-location.href=
-"admin.html";
-
-
-}
+location.href="admin.html";
 
 }else{
 
-location.href=
-"admin.html";
+message.innerText="IDまたはパスワードが違います";
 
 }
-
-
-
-}else{
-
-
-message.innerText =
-"IDまたはパスワードが違います";
-
-
-}
-
 
 
 }catch(e){
 
-
 console.error(e);
-
-
-message.innerText =
-"通信エラー";
-
+message.innerText="通信エラー";
 
 }
 
-
-
 };
-
 
 });
